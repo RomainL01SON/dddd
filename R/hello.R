ac0d306aac1b5981bc6a17d91871a66fec5cc9e7
Romain Loison <32923987+RomainL01SON@users.noreply.github.com>
2021-12-02 09:04:11 +0000

    Update hello.R
    Set 0 as min GDD if (Tmin +Tmax)/2 < Tbase

--- R/hello.R
+++ R/hello.R
@@ -13,11 +13,6 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
-}
-
-# Rastafari
 #' Growing degree days
 #'
 #' This function computes growing degree days and its sum
@@ -32,7 +27,7 @@ GDD_cirad <- function(Tbase,Tmin,Tmax){
   gdd <- NULL
   for(i in 1:length(Tmin)){
     ifelse(Tmin[i]<Tmax[i],
-           gdd[i] <- (Tmin[i]+Tmax[i])/2-Tbase,stop(paste0("You have at least a value of Tmin >= Tmax : data line number ", i)))
+           gdd[i] <- max(0,(Tmin[i]+Tmax[i])/2-Tbase),stop(paste0("You have at least a value of Tmin >= Tmax : data line number ", i)))
   }
   cum_gdd <- cumsum(gdd)
   return(list("GDD"=gdd,"cumGDD"=cum_gdd))
